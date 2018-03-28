e8f8a0899a26d3023809341e051a9b30767a2565
siddharth sharma <sandesh251189@gmail.com>
2018-03-28 05:51:53 -0400

    Session One compleate

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDom from 'react-dom'
+import SearchBar from './components/search_bar';
+const API_KEY='AIzaSyAvouBC__uLoHpzonLkDGFF2HVRz1N-TPA'
 
 
-const App=()=>{
-  return <div>Hii</div>;
-}
 
 
-ReactDom.render(<App />,document.querySelector('.container'))
+
+
+ReactDom.render(<SearchBar />,document.querySelector('.container'))
